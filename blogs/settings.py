c08b8cc71c967deb1c8d41c1b030bc4723c243f6
ayushyadav9 <ay58679@gmail.com>
2021-05-27 23:34:43 +0530

    .

--- blogs/settings.py
+++ blogs/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'wmirx@9zc^(_e0mkp3v@9+fb*jyo4+rh63x9!kc4q_&se)2i9m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
