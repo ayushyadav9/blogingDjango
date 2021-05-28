f6e9f14186046e736e567b6a6017ae8001a6782b
ayushyadav9 <ay58679@gmail.com>
2021-05-28 14:46:45 +0530

    .

--- blogs/settings.py
+++ blogs/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'wmirx@9zc^(_e0mkp3v@9+fb*jyo4+rh63x9!kc4q_&se)2i9m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'blogs-ayush.herokuapp.com']
 
